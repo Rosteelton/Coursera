e27221121b33911327fc0cc7e3ec2e35c86134b0
Rosteelton <Solovyev.Anton.dev@gmail.com>
2016-07-21 18:15:47 +0300

    course #1 completed

---
+++ functional-programming-principles/forcomp/src/main/scala/forcomp/App.scala
@@ -0,0 +1,16 @@
+package forcomp
+import Anagrams._
+
+object App extends App {
+
+ // val x = List(('a', 1), ('d', 1), ('l', 1), ('r', 1))
+ // val y = List(('r', 1))
+  println(combinations(List(('a',2),('b',2))))
+  println(combinations(List(('a',2),('b',2),('c',1))))
+  //println(subtract(x,y))
+
+
+  //println(sentenceAnagrams(List("oy", "yo")))
+  (1 to 5).toArray.sliding(2)
+
+}
