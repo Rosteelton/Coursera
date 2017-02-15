35ecd188c08bc0c7a78ca5445d4172352711f0b4
Rosteelton <Rosteelton@gmail.com>
2017-02-15 22:17:25 +0300

    Add #6 task (spark sql)

--- spark-udemy/src/main/scala/AverageFriendsByName.scala
+++ spark-udemy/src/main/scala/AverageFriendsByName.scala
@@ -1,5 +1,7 @@
 import common.SparkStuff
 import org.apache.spark.SparkContext
+import org.apache.spark.sql.{Column, DataFrame, Dataset, SparkSession}
+
 
 object AverageFriendsByName extends App with SparkStuff {
 
@@ -20,7 +22,34 @@ object AverageFriendsByName extends App with SparkStuff {
       .reduceByKey((x, y) => (x._1 + y._1, x._2 + y._2))
       .mapValues(pair => pair._1 / pair._2)
 
-  val result = flow.collect().sortBy(_._1).toSeq
+  val result = flow.collect().sortBy(_._2).reverse.toSeq
 
   result.foreach(println)
+
+  //DATASET API
+
+  val sparkConf = SparkSession
+    .builder()
+    .appName("Friends")
+    .master("local[*]")
+    .getOrCreate()
+
+  import sparkConf.implicits._
+
+case class Friend(name: String, friendsNumber: Int)
+  def getNameAndFriends2: String => Friend = {
+    s =>
+      val name: String = s.split(",")(1)
+      val friends: Int = s.split(",")(3).toInt
+      Friend(name, friends)
+  }
+  val ds: Dataset[Friend] = rdd.map(getNameAndFriends2).toDS()
+
+  import org.apache.spark.sql.functions._
+  ds.groupBy("name").avg("friendsNumber").orderBy(desc("avg(friendsNumber)")).show(50)
+
+
+
+
+
 }
